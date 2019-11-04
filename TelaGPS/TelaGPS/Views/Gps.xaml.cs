3b857c45063506cd3235cffe97eed5ee95b67f55
igor <igormartuci99@gmail.com>
2019-11-03 21:18:15 -0300

    tela gps

---
+++ TelaGPS/TelaGPS/Views/Gps.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TelaGPS.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Gps : ContentPage
+	{
+        public Gps()
+        {
+            InitializeComponent();
+
+            //  xamarin.forms.maps
+
+
+            // xamarin.forms.googlemaps
+            Xamarin.Forms.GoogleMaps.Map map = new Xamarin.Forms.GoogleMaps.Map();
+            map.MyLocationEnabled = true;
+            map.UiSettings.MyLocationButtonEnabled = true;
+        }
+    }
+}
---
+++ TelaGPS/TelaGPS/Views/OpcoesRotas.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelaGPS.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TelaGPS.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OpcoesRotas : ContentPage
+	{
+		public OpcoesRotas ()
+		{
+			InitializeComponent ();
+
+		}
+
+        public void onCalcularRota ()
+        {
+            if (txtOrigem.Text != null && txtDestino != null)
+            {
+                App.Current.MainPage = new Gps();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
